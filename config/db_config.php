d8d7688c3a054ca3d8f157b9dc07157b4d3bc9fc
AntonioValentin <71309949+AntonioValentinSayago@users.noreply.github.com>
2023-08-22 20:03:13 -0600

    Update db_config.php

--- config/db_config.php
+++ config/db_config.php
@@ -3,7 +3,7 @@
 
         /* Connecting to the database. */
         $servidor = "localhost";
-        $usuario = "root";
+        $usuario = "u551598332_root";
         $contraseña = "#iSmT:[>8";
         $base_de_datos = "u551598332_lavanderia_sap";
         $conexion = mysqli_connect($servidor, $usuario, $contraseña, $base_de_datos);
