65f44631c78d6564d30d4b4cdf47c3f2571d38e0
AntonioValentin <71309949+AntonioValentinSayago@users.noreply.github.com>
2023-08-22 20:16:30 -0600

    Update guardarEmpleado.php

--- systemUser/guardarEmpleado.php
+++ systemUser/guardarEmpleado.php
@@ -8,7 +8,7 @@
     $cargo = $_POST["cargo"];
     $numEmp = $_POST["numEmp"];
 
-    $insertar = "INSERT INTO ctl_userSystem (nombreCompleto, apellidoCompleto,email_correo, num_empleado, telefono, foto_perfil, clave, cargo, fecha_entrada, fecha_salida)
+    $insertar = "INSERT INTO ctl_usersystem (nombreCompleto, apellidoCompleto,email_correo, num_empleado, telefono, foto_perfil, clave, cargo, fecha_entrada, fecha_salida)
                 VALUES ('$nombreEmp','$lastEmp','$email','$numEmp','$telefono','https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png','clave789','$cargo','2022-01-01','2022-01-01')";
     $insertar = mysqli_query($conexion, $insertar);
 
