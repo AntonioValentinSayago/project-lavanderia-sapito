c65b3a09d4cb412583fb152331f9b6d7ac366104
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-21 09:48:18 -0600

    Nuevos Cambios

---
+++ pdf/pedidos/consultaFecha/fecha.php
@@ -0,0 +1,29 @@
+<?php 
+
+require_once '../../../mdpdf/vendor/autoload.php';
+require_once 'ejemplo.php';
+
+$fecha = $_POST['fecha'];
+
+$mysqli= new mysqli("localhost","u551598332_root","#iSmT:[>8","u551598332_lavanderia_sap");
+$mysqli->set_charset("utf8");
+$consulta=$mysqli->prepare("SELECT * from ctl_historial WHERE fecha_entrega = '$fecha'");
+$consulta->execute();
+$resultados=$consulta->get_result();
+$productos=array();
+while($row=$resultados->fetch_assoc()) $productos[]=$row;
+$mysqli->close();
+
+
+
+$mpdf = new \Mpdf\Mpdf();
+$css = file_get_contents("../../style.css");
+
+$plantilla = getPlantilla($productos);
+$mpdf->WriteHTML($css,\Mpdf\HTMLParserMode::HEADER_CSS);
+$mpdf->writeHTML($plantilla,\Mpdf\HTMLParserMode::HTML_BODY); 
+$mpdf->setFooter('{PAGENO}/{nbpg}');
+
+$mpdf->Output("miarchivopdf","I");
+
+?>
