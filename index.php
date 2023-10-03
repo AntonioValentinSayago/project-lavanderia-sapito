fee68820caf3c5b9ea4377074751972395c57d6b
AntonioValentin <cesar_anto30@hotmail.com>
2023-10-03 17:06:00 -0600

    Nuevos cambios

--- index.php
+++ index.php
@@ -33,49 +33,41 @@ if (isset($_SESSION['id'])) {
 <body">
   <!-- Formulario Login -->
   <div class="d-lg-flex half">
-      <div
-        class="bg order-1 order-md-2"
-        style="background-image: url('https://wallpapers.com/images/hd/laundry-room-pictures-swc19ed2it6n8r9c.jpg')"
-      ></div>
+    <div class="bg order-1 order-md-2"
+      style="background-image: url('https://wallpapers.com/images/hd/laundry-room-pictures-swc19ed2it6n8r9c.jpg')">
+    </div>
 
     <div class="contents order-2 order-md-1">
       <div class="container">
         <!-- Estructura del formulario -->
         <div class="row align-items-center justify-content-center">
-            <div class="col-md-7">
-              <h3>Lavandería<strong>Sapito</strong></h3>
-              <div class="form-group first">
-                  <label for="username">Correo Electronico</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="your-email@gmail.com"
-                    id="user"
-                  />
-              </div>
-              <div class="form-group last mb-3">
-                  <label for="password">Contraseña</label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    placeholder="****************"
-                    id="clave"
-                    autocomplete="off"
-                  />
-              </div>
-              <!-- Animacion de load (solo sera visible cuando el cliente espere una respuesta del servidor )-->
-              <div class="row" id="load" hidden="hidden">
-                <div class="col-xs-4 col-xs-offset-4 col-md-2 col-md-offset-5">
-                  <img src="img/load.gif" width="100%" alt="">
-                </div>
-                <div class="col-xs-12 center text-accent">
-                  <span>Validando información...</span>
-                </div>
-              </div>
-              <!-- Fin load -->
-              <button type="button" class="btn btn-enviar btn-block" name="button" id="login">Iniciar sesion</button>
-
+          <div class="col-md-7">
+            <div class="alert alert-warning" role="alert">
+              Servicio en Mantenimiento :)
             </div>
+            <h3>Lavandería<strong>Sapito</strong> <h6>V2.0.0</h6> </h3>
+            <div class="form-group first">
+              <label for="username">Correo Electronico</label>
+              <input type="text" class="form-control" placeholder="your-email@gmail.com" id="user" />
+            </div>
+            <div class="form-group last mb-3">
+              <label for="password">Contraseña</label>
+              <input type="password" class="form-control" placeholder="****************" id="clave"
+                autocomplete="off" />
+            </div>
+            <!-- Animacion de load (solo sera visible cuando el cliente espere una respuesta del servidor )-->
+            <div class="row" id="load" hidden="hidden">
+              <div class="col-xs-4 col-xs-offset-4 col-md-2 col-md-offset-5">
+                <img src="img/load.gif" width="100%" alt="">
+              </div>
+              <div class="col-xs-12 center text-accent">
+                <span>Validando información...</span>
+              </div>
+            </div>
+            <!-- Fin load -->
+            <button type="button" class="btn btn-enviar btn-block" name="button" id="login">Iniciar sesion</button>
+
+          </div>
         </div>
       </div>
     </div>
