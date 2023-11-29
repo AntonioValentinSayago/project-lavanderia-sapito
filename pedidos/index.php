bcc1a5b3985fc867b02d37cb3fbc9b8a2c4bc394
AntonioValentin <cesar_anto30@hotmail.com>
2023-11-28 19:08:37 -0600

    Cambios para la Nueva Versión

--- pedidos/index.php
+++ pedidos/index.php
@@ -356,7 +356,7 @@ require_once("../config/db_config.php");
                   <button type="button" class="btn" onclick="alertaMensaje()"  style="background:#34d399; color:#f0fdf4" ><i class="bi bi-save"></i>
                     Crear Pedido</button>
                   <button type="reset" onclick="resetFormulario()" class="btn btn-default" style="background: #991b1b; color:white"><i
-                      class="bi bi-trash3"></i></button>
+                      class="bi bi-trash3"></i> Cancelar Pedido</button>
                 </div>
               </form><!-- End No Labels Form -->
             </div>
