bf79149bfa5b3d3d61d1dc7dcd25d011968f74f8
AntonioValentin <cesar_anto30@hotmail.com>
2023-12-19 19:20:27 -0600

    Version 2.0.0

--- categorias/addCategoria.php
+++ categorias/addCategoria.php
@@ -81,10 +81,8 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
                 <li class="nav-item dropdown pe-3">
 
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                        <img src="<?php echo ucfirst($_SESSION['img']); ?>"
-                            alt="<?php echo ucfirst($_SESSION['nombre']); ?>" class="rounded-circle">
                         <span class="d-none d-md-block dropdown-toggle ps-2">
-                            <?php echo ucfirst($_SESSION['nombre']); ?>
+                            <?php echo $_SESSION['email']; ?>
                         </span>
                     </a><!-- End Profile Iamge Icon -->
 
@@ -93,7 +91,6 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
                             <h6>
                                 <?php echo ucfirst($_SESSION['nombre']); ?>
                             </h6>
-                            <span>Administracion</span>
                         </li>
                         <li>
                             <hr class="dropdown-divider">
@@ -133,13 +130,27 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
             <li class="nav-item">
                 <a class="nav-link collapsed" href="../pedidos/index.php">
                     <i class="bi bi-grid"></i>
-                    <span>Panel Principal</span>
+                    <span>Crear Nueva Nota</span>
                 </a>
             </li><!-- End Dashboard Nav -->
 
             <li class="nav-item">
+        <a class="nav-link collapsed" href="../pedidos/pedidos-activos.php">
+          <i class="bi bi-grid"></i>
+          <span>Control de Pedidos Activos</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="../consultaPedido/index.php">
+          <i class="bi bi-grid"></i>
+          <span>Historia de Notas Entregadas</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+            <li class="nav-item">
                 <a class="nav-link collapsed" href="../inventario/inventario.php">
-                    <i class="bi bi-menu-button-wide"></i><span>Inventario</span>
+                    <i class="bi bi-menu-button-wide"></i><span>Inventario de Productos</span>
                 </a>
             </li><!-- End Components Nav -->
 
@@ -152,13 +163,13 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
 
             <li class="nav-item">
                 <a class="nav-link collapsed" data-bs-target="#tables-nav">
-                    <i class="bi bi-layout-text-window-reverse"></i><span>Reportes</span>
+                    <i class="bi bi-layout-text-window-reverse"></i><span>Gestión de Reportes/Reportes diarios</span>
                 </a>
             </li><!-- End Tables Nav -->
 
             <li class="nav-item">
                 <a class="nav-link collapsed" href="../gastos/index.php">
-                    <i class="bi bi-bar-chart"></i><span>Gastos Generales</span>
+                    <i class="bi bi-bar-chart"></i><span>Control de Gastos Generales</span>
                 </a>
             </li><!-- End Charts Nav -->
 
@@ -166,14 +177,14 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
             <li class="nav-item">
                 <a class="nav-link collapsed" href="../clientes/clientes.php">
                     <i class="bi bi-person"></i>
-                    <span>Clientes</span>
+                    <span>Control de Clientes</span>
                 </a>
             </li><!-- End Profile Page Nav -->
 
             <li class="nav-item">
                 <a class="nav-link collapsed" href="../systemUser/index.php">
                     <i class="bi bi-person-add"></i>
-                    <span>Empleados</span>
+                    <span>Control de Empleados</span>
                 </a>
             </li><!-- End Profile Page Nav -->
 
@@ -186,7 +197,7 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
             <li class="nav-item">
                 <a class="nav-link collapsed" href="../login/controller/cerrarSesion.php">
                     <i class="bi bi-box-arrow-right"></i>
-                    <span>Cerrar Sesión</span>
+                    <span>Salir del Sistema</span>
                 </a>
             </li><!-- End F.A.Q Page Nav -->
 
@@ -259,7 +270,7 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
             &copy; Copyright <strong><span>Lavanderia_Sapito</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-            Designed by <a href="https://bootstrapmade.com/">Lavandera Sapito </a>
+            Designed by <a href="https://bootstrapmade.com/">Lavandería Sapito 2024</a>
         </div>
     </footer><!-- End Footer -->
 
