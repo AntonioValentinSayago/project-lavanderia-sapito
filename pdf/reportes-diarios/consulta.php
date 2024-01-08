cc0d76fdf2dbabb77d8f9ce5d3b0a8ef45b660dd
AntonioValentin <cesar_anto30@hotmail.com>
2024-01-08 14:18:20 -0600

    version 2.0.3

--- pdf/reportes-diarios/consulta.php
+++ pdf/reportes-diarios/consulta.php
@@ -2,7 +2,7 @@
 
 $mysqli= new mysqli("localhost","u551598332_root","#iSmT:[>8","u551598332_lavanderia_sap");
 $mysqli->set_charset("utf8");
-$consulta=$mysqli->prepare("SELECT ctl_reporte.fecha_reporte, ctl_reporte.ingreso_total_diario, ctl_reporte.id_ctl_ventapedidos 
+$consulta=$mysqli->prepare("SELECT ctl_reporte.fecha_reporte, ctl_reporte.ingreso_total_diario, ctl_reporte.id_ctl_ventapedidos, 
                             ped.folio_nota, ped.hora_entrega
                             from ctl_reporte_diarios ctl_reporte
                             JOIN ctl_ventapedidos ped ON ped.id_ctl_ventapedidos = ctl_reporte.id_ctl_ventapedidos");
