6de55a0f0ff627236ad412819614ee945d6c4584
AntonioValentin <cesar_anto30@hotmail.com>
2024-02-04 22:37:18 -0600

    Version 1.0.0 - Reportes

--- pdf/pedidos/consultaFecha/fecha.php
+++ pdf/pedidos/consultaFecha/fecha.php
@@ -5,26 +5,25 @@ require_once 'ejemplo.php';
 
 $fecha = $_POST['fecha'];
 
-echo $fecha;
-// $mysqli= new mysqli("localhost","u551598332_root","#iSmT:[>8","u551598332_lavanderia_sap");
-// $mysqli->set_charset("utf8");
-// $consulta=$mysqli->prepare("SELECT * from ctl_historial WHERE fecha_entrega = '$fecha'");
-// $consulta->execute();
-// $resultados=$consulta->get_result();
-// $productos=array();
-// while($row=$resultados->fetch_assoc()) $productos[]=$row;
-// $mysqli->close();
+$mysqli= new mysqli("localhost","u551598332_root","#iSmT:[>8","u551598332_lavanderia_sap");
+$mysqli->set_charset("utf8");
+$consulta=$mysqli->prepare("SELECT * from ctl_historial WHERE fecha_entrega = '$fecha'");
+$consulta->execute();
+$resultados=$consulta->get_result();
+$productos=array();
+while($row=$resultados->fetch_assoc()) $productos[]=$row;
+$mysqli->close();
 
 
 
-// $mpdf = new \Mpdf\Mpdf();
-// $css = file_get_contents("../../style.css");
+$mpdf = new \Mpdf\Mpdf();
+$css = file_get_contents("../../style.css");
 
-// $plantilla = getPlantilla($productos);
-// $mpdf->WriteHTML($css,\Mpdf\HTMLParserMode::HEADER_CSS);
-// $mpdf->writeHTML($plantilla,\Mpdf\HTMLParserMode::HTML_BODY); 
-// $mpdf->setFooter('{PAGENO}/{nbpg}');
+$plantilla = getPlantilla($productos);
+$mpdf->WriteHTML($css,\Mpdf\HTMLParserMode::HEADER_CSS);
+$mpdf->writeHTML($plantilla,\Mpdf\HTMLParserMode::HTML_BODY); 
+$mpdf->setFooter('{PAGENO}/{nbpg}');
 
-// $mpdf->Output("miarchivopdf","I");
+$mpdf->Output("miarchivopdf","I");
 
 ?>
