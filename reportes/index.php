b6305cb419246ddbe16203e74bbac3acfeab57c9
AntonioValentin <cesar_anto30@hotmail.com>
2024-02-13 21:32:39 -0600

    Version 2.0.1 - Update Reportes

--- reportes/index.php
+++ reportes/index.php
@@ -224,12 +224,14 @@ if (!isset($_SESSION['cargo'])) {
                     </div>
                     <div class="ps-3">
                       <?php
+                      // Establecer la zona horaria
+                      date_default_timezone_set('America/Mexico_City');
                       $fecha_reporte_diario = date('Y-m-d');
 
                       // Realizar la consulta SQL
                       $consulta = "SELECT SUM(ingreso_total_diario) AS total_ingreso, fecha_reporte
                         FROM ctl_reporte_diarios
-                        WHERE DATE(fecha_reporte) = date('Y-m-d')
+                        WHERE DATE(fecha_reporte) = '$fecha_reporte_diario'
                         GROUP BY fecha_reporte";
                       $stmt = mysqli_query($conexion, $consulta);
 
