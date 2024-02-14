1dea07a4efd4d01af69187c0cf4d6d8222748078
AntonioValentin <cesar_anto30@hotmail.com>
2024-02-13 21:20:28 -0600

    Version 2.0.1 - Update Reportes

--- reportes/index.php
+++ reportes/index.php
@@ -227,7 +227,7 @@ if (!isset($_SESSION['cargo'])) {
                       $fecha_reporte_diario = date('Y-m-d');
                       $consulta = "SELECT SUM(ingreso_total_diario) AS total_ingreso, fecha_reporte
               FROM ctl_reporte_diarios
-              WHERE DATE(fecha_reporte) = '$fecha_reporte_diario'
+              WHERE DATE(fecha_reporte) = '2024-02-13'
               GROUP BY fecha_reporte";
                       $stmt = mysqli_query($conexion, $consulta);
 
