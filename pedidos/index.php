1796ed122ea0de40e63053129230b4f97be293b6
AntonioValentin <cesar_anto30@hotmail.com>
2024-04-08 20:40:26 -0600

    Cambios a la version 2.0.4

--- pedidos/index.php
+++ pedidos/index.php
@@ -600,7 +600,7 @@ require_once ("../config/db_config.php");
       var resta = document.getElementById("resta").value;
       //* validacion para no insertar datos mayores o menor al dinero total a pagar
       if (dineroCuenta > resta) {
-        console.log("object");
+        dineroCuenta = " ";
       }
     }
   </script>
