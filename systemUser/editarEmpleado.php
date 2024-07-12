e3f7abea998d9ca135f919fe54669ca43343e04a
cesarvalentinsayago <cesarvalentinsayago@gmail.com>
2024-07-12 13:16:38 -0600

    feat:[Version 3.0 120624]

--- systemUser/editarEmpleado.php
+++ systemUser/editarEmpleado.php
@@ -257,10 +257,10 @@ if (!isset($_SESSION['cargo']) || $_SESSION['cargo'] != 1) {
                                                 <div class="text-center">
                                                     <button type="submit" class="btn btn-success"
                                                             style="background:#34d399; color:#f0fdf4"><i
-                                                            class="bi bi-save-fill"></i> Guardar</button>
+                                                            class="bi bi-save-fill"></i> Editar Empleado</button>
                                                     <a href="index.php"><button type="button"
                                                             class="btn btn-secondary"
-                                                            style="background: #991b1b; color:white">Cancelar</button></a>
+                                                            style="background: #991b1b; color:white">Cancelar/regresar</button></a>
                                                 </div>
                                             </form><!-- End No Labels Form -->
                                             <?php
