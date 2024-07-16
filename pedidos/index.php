8877fb7c5be818fcbdd1f07207d0622f723fa663
cesarvalentinsayago <cesarvalentinsayago@gmail.com>
2024-07-16 11:41:17 -0600

    [feat]: Version 3.0

--- pedidos/index.php
+++ pedidos/index.php
@@ -187,9 +187,6 @@ require_once ("../config/db_config.php");
   </aside><!-- End Sidebar-->
 
   <main id="main" class="main">
-    <div class="alert alert-warning" role="alert">
-      Sitio web en mantenimiento
-    </div>
     <div class="pagetitle" style="display: flex;">
       <h1>Crear Nueva Venta</h1>
       <nav>
